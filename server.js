54a263dd0fdc5e5322da2c313757046d09402bc8
Kenford <kzhou0066@gmail.com>
2019-06-03 05:01:24 -0500

    update dist path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/epic7-catalyst-tool'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/epic7-catalyst-tool/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
